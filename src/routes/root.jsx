31a75fd0fad0fcc67a9b316438442370a6dfdf27
HigorR456 <higor.ruan456@gmail.com>
2023-12-12 13:14:10 -0300

    install button added

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,7 +3,21 @@ import React, { useEffect, useState } from 'react';
 import {Helmet} from "react-helmet";
 
 export default function Root() {
-  
+  let deferredPrompt;
+  window.addEventListener('beforeinstallprompt', (e) => {
+      deferredPrompt = e;
+  });
+
+  const installApp = async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null;
+        }
+    }
+  };
+
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
@@ -21,6 +35,10 @@ export default function Root() {
       </Helmet>
         <div id="sidebar">
           <h1>React Router Index</h1>
+          <button onClick={() => installApp()}>Install App</button>
+          <span style={{"background": "red", "color": "black"}}>
+            Obs: feature experimental, não tem suporte no Safari e Firefox
+          </span>
           <nav>
             <ul>
               <li>
